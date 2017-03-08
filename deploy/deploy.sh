783a64a87e83800fdb45e012032776639640add7
RK <romainknezevic@gmail.com>
2017-03-08 13:34:29 +0100

    add deploy folder, some scripts & builds

---
+++ deploy/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="0.2"
+
+deploy_win32_jdidco() {
+  GOOS=windows GOARCH=386 go build -v -o lbcbot.exe jdid.co/lbcbot || exit 1
+  test -x lbcbot.exe || exit 1
+
+	tmp=$(mktemp -d)
+  src=$tmp/lbcbot/
+  mkdir -p $src
+
+
+	target="lbcbot_win32-v${VERSION}.zip"
+  cp -a README.md config_sample.json lbcbot.exe $src/
+  echo "zipping $target"
+  zip -r $target $src
+  rm -rf $tmp
+
+  echo "sending $target to remote.."
+	scp $target wservices:share/
+}
+
+deploy_win32_jdidco
