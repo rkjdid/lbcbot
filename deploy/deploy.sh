08bc85f930f2ba679f2a77510586fdb34b9698f7
RK <romainknezevic@gmail.com>
2017-03-09 13:38:00 +0100

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.3"
+VERSION="0.4d"
 
 if [ "$(basename $(pwd))" != "deploy" ]; then
 	echo "must be in deploy dir" >&2
@@ -11,19 +11,21 @@ deploy_win32_jdidco() {
   GOOS=windows GOARCH=386 go build -v -o lbcbot.exe jdid.co/lbcbot || exit 1
   test -x lbcbot.exe || exit 1
 
-	tmp=$(mktemp -d)
+  tmp=$(mktemp -d)
   src=$tmp/lbcbot/
   mkdir -p $src
 
-
-	target="lbcbot_win32-v${VERSION}.zip"
-  cp -a ../README.md ../config_sample.json lbcbot.exe $src/
+  target="lbcbot_win32-v${VERSION}.zip"
+  cp -ar ../README.md ../config_sample.json ../html lbcbot.exe $src/
   echo "zipping $target"
-  zip -r $target $src
+  cd $(dirname $src)
+  zip -r $target lbcbot/
+  cd -
+  mv $(dirname $src)/$target .
   rm -rf $tmp
 
   echo "sending $target to remote.."
-	scp $target wservices:share/
+  scp $target wservices:share/
 }
 
 deploy_win32_jdidco
